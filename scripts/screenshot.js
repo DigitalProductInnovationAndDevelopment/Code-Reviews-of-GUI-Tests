ec21c34b439842951a359be6ee8d4bec1ad129fe
amarilda611 <amarildamemushaj1@gmail.com>
2025-05-29 15:34:12 +0200

    Update screenshot.js

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.resolve(__dirname, '../tests_artifacts');
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
+
 const { chromium } = require('playwright');
 
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto('http://localhost:3000');
-  await page.screenshot({ path: 'tests_artifacts/screenshot.png' });
+  await page.screenshot({ path: path.join(dir, 'screenshot.png') });
   await browser.close();
 })();
