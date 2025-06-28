a690073f695f813a1e7d765253a09e6a9409edd0
ist1102500 <alice.arnaud.mota@tecnico.ulisboa.pt>
2025-06-28 19:58:04 +0200

    feat: eslint test file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
+
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import playwrightPlugin from "eslint-plugin-playwright";
+import prettierPlugin from "eslint-plugin-prettier";
 
 let localOverride = {};
 
 try {
-  localOverride = (await import("./eslint.local.config.js")).default;
-  console.log("✅ Loaded local ESLint override config from root.");
+  localOverride = (await import("./.eslint.local.config.js")).default;
+  console.log("✅ Loaded local ESLint override config.");
 } catch (err) {
-  // No root override
+  console.log("ℹ️ No local override config found.");
 }
 
 export default [
@@ -31,13 +34,15 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
+      playwright: playwrightPlugin,
+      prettier: prettierPlugin,
     },
     rules: {
-      "no-unused-vars": "off", 
-      "@typescript-eslint/no-unused-vars": "error",
+      "no-unused-vars": "error",
+      "playwright/no-wait-for-timeout": "error",
       "@typescript-eslint/no-explicit-any": "off",
       "no-console": "off",
-      
+      "prettier/prettier": "error",
       ...(localOverride?.rules || {}),
     },
   },
