88e10767dc2b6fcfd31d89428615406783bb3616
kalpaghada <93324465+kalpaghada@users.noreply.github.com>
2025-06-29 22:03:12 +0200

    Revert "feat: eslint test file (#101)"
    This reverts commit f7f14cdf0f2e3a692b71a29a75940aa13b1f081b.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,13 @@
-
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
-import playwrightPlugin from "eslint-plugin-playwright";
-import prettierPlugin from "eslint-plugin-prettier";
 
 let localOverride = {};
 
 try {
-  localOverride = (await import("./.eslint.local.config.js")).default;
-  console.log("✅ Loaded local ESLint override config.");
+  localOverride = (await import("./eslint.local.config.js")).default;
+  console.log("✅ Loaded local ESLint override config from root.");
 } catch (err) {
-  console.log("ℹ️ No local override config found.");
+  // No root override
 }
 
 export default [
@@ -34,15 +31,13 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
-      playwright: playwrightPlugin,
-      prettier: prettierPlugin,
     },
     rules: {
-      "no-unused-vars": "error",
-      "playwright/no-wait-for-timeout": "error",
+      "no-unused-vars": "off", 
+      "@typescript-eslint/no-unused-vars": "error",
       "@typescript-eslint/no-explicit-any": "off",
       "no-console": "off",
-      "prettier/prettier": "error",
+      
       ...(localOverride?.rules || {}),
     },
   },
